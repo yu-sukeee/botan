b722bc2aecb39fc696f5b40f8276b0c4511483f7
yu-sukeee <ysk.watch66@gmail.com>
2020-11-30 10:14:42 +0900

    Updates

--- precache-manifest.8c7d618b57b2eac8ad5fc54637127641.js
+++ precache-manifest.8c7d618b57b2eac8ad5fc54637127641.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "58bf401d018df27cbb5f034cc80bfe92",
+    "revision": "8ee7c002a8d4f5dfbb66f0794d5788cd",
     "url": "/botan/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/botan/static/css/2.05d56aed.chunk.css"
   },
   {
-    "revision": "94396bf5c828c653550e",
+    "revision": "e1deb068a0552c226a79",
     "url": "/botan/static/css/main.40c151ac.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/botan/static/js/2.28d33d05.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94396bf5c828c653550e",
-    "url": "/botan/static/js/main.cc6cae3c.chunk.js"
+    "revision": "e1deb068a0552c226a79",
+    "url": "/botan/static/js/main.73f192bd.chunk.js"
   },
   {
     "revision": "e5eb26451e5a08ec0fff",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/botan/precache-manifest.cb6e4c020d27c4153e99f5f752b7d428.js"
+  "/botan/precache-manifest.8c7d618b57b2eac8ad5fc54637127641.js"
 );
 
 self.addEventListener('message', (event) => {
